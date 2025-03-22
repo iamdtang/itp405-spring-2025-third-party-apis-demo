de188fed0a9120c68624512b6673c1e3a101c8ae
David Tang <687449+iamdtang@users.noreply.github.com>
2025-03-22 14:35:47 -0700

    Yelp API demo

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,33 @@ Route::get('/itunes', function (Request $request) {
         'response' => $response,
     ]);
  });
+
+Route::get('/yelp', function () {
+    // Using the Business Search API: https://docs.developer.yelp.com/reference/v3_business_search
+    $queryString = http_build_query([
+        'term' => 'vegan',
+        'location' => 'Los Angeles',
+    ]);
+
+    // We can set headers for our request using withHeaders()
+    // https://laravel.com/docs/12.x/http-client#headers
+    // Sending our API key in our request under the `Authorization` header.
+    // return Http::withHeaders([
+    //     'Authorization' => "Bearer " . env('YELP_API_KEY')
+    // ])
+    //     ->get("https://api.yelp.com/v3/businesses/search?$queryString")
+    //     ->json();
+    
+    $cacheKey = "yelp-api-$queryString";
+    $seconds = 60;
+
+    $response = Cache::remember($cacheKey, $seconds, function () use ($queryString) {
+        // OR you can use withToken to set the Authorization header
+        // https://laravel.com/docs/12.x/http-client#bearer-tokens
+        return Http::withToken(env('YELP_API_KEY'))
+            ->get("https://api.yelp.com/v3/businesses/search?$queryString")
+            ->object();
+    });
+
+    dd($response);
+});
